3a082108c63fee40e9c7b29422c6d6da5d6f220d
landiao <zhangwangzhe@icloud.com>
2017-03-14 21:51:24 +0800

    这个是我最想的

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
-<<<<<<< HEAD
+
 echo '蓝调';
-?>
-=======
-<<<<<<< HEAD
+
 /**
  * Created by PhpStorm.
  * User: zhyunfe
@@ -11,8 +9,8 @@ echo '蓝调';
  * Time: 下午8:12
  */
 var_dump('nothing to show');
-=======
+
+var_dump('asdfasdfasdfasdfasdfasf');
 
 ?>
->>>>>>> 192b496b33b14f3bc463ee38ce66c9252f633810
->>>>>>> 40b83929ab937101c703626ba9535fcd8f68d076
+
